88fdd5cc6c7df9f47e331abb9d3b601e24843f5b
saieainkhant <71281504+iamyogurthh@users.noreply.github.com>
2023-06-18 19:22:09 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,179 +1,100 @@
-/*
-const trackList = [
-    {
-        trackName: "Circle",
-        artistName: "Post Malone",
-        trackDuration: "3:36",
-        img: "circle"
-    },
-    {
-        trackName: "Flower",
-        artistName: "Jisoo",
-        trackDuration: "3:05",
-        img: "flower"
-    },
-    {
-        trackName: "You Belong With Me",
-        artistName: "Taylar Swift",
-        trackDuration: "3:49",
-        img: "youBelongWithMe"
-    },
-    {
-        trackName: "Gone",
-        artistName: "Rose",
-        trackDuration: "3:41",
-        img: "gone"
-    },
-    {
-        trackName: "Anyone",
-        artistName: "Justin Bieber",
-        trackDuration: "3:11",
-        img: "anyone"
-    },
-    {
-        trackName: "Icecream",
-        artistName: "Blackpink",
-        trackDuration: "3:03",
-        img: "icecream"
-    },
-    {
-        trackName: "Sweater Weather",
-        artistName: "The Neighbourhood",
-        trackDuration: "3:57",
-        img: "sweaterWeather"
-    },
-    {
-        trackName: "Everything Goes On",
-        artistName: "Porter Robinson",
-        trackDuration: "2:04",
-        img: "everythingGoesOn"
-    },
-    {
-        trackName: "Viva La Vida",
-        artistName: "Coldplay",
-        trackDuration: "4:35",
-        img: "vivaLaVida"
-    },
-    {
-        trackName: "Whisper",
-        artistName: "Boombox Cartel",
-        trackDuration: "4:04",
-        img: "whisper"
-    }
-];
-*/
 const trackList = [];
 
 //api handel
-const url = 'https://itunes.apple.com/search?term=blackpink';
+const url = `https://itunes.apple.com/search?term=blackpink`;
 fetch(url)
     .then((Response) => Response.json())
     .then((data) => {
         console.log(data.results);
         const artists = data.results;
-        return artists.map(result=>{
-            
-            /*
-            const article = document.createElement("article"),
-                artists = document.createElement('p'),
-                song = document.createElement('h4'),
-                img = document.createElement('img'),
-                audio = document.createElement('audio'),
-                audioSource = document.createElement('source')
+        artists.forEach((result) => {
+            const trackObject = {
+                trackName: result.trackName,
+                artistName: result.artistName,
+                trackDuration: result.trackDuration,
+                img: result.artworkUrl100,
+                genre: result.primaryGenreName
+            };
 
-            console.log(result); 
+            trackList.push(trackObject);
+        });
 
-            artists.innerHTML = result.artistName;
-            song.innerHTML = result.trackName;
-            img.src = result.artworkUrl100;
-            audioSource.src = result.previewUrl;
-            audio.controls = true;
-
-            article.appendChild(img);
-
-            article.appendChild(img);
-            article.appendChild(artists);
-            article.appendChild(song);
-            article.appendChild(audio);
-            */
-        }) 
-        
+        renderTrackList();
     })
-    .catch(error=>console.log("Request fail:", error))
+    .catch(error => console.log("Request fail:", error));
 
-
-//render the music list on the DOM
-function renderTrackList(index) {
+// render the music list on the DOM
+function renderTrackList() {
     let trackListHTML = "";
 
-    for(let i = 0; i < trackList.length; i++) {
+    for (let i = 0; i < trackList.length; i++) {
         const trackObject = trackList[i];
-        const {trackName,artistName,trackDuration,img} = trackObject;
+        const { trackName, artistName, trackDuration, img, genre } = trackObject;
 
-        const html =`
+        const html = `
             <div class="song-container">
                 <div class="song-stats">
                     <div class="song-image-continer">
-                        <img src="/images/song-cover-img/${img}.png">
+                        <img src="${img}">
                     </div>
                     <div class="song-name-container">
                         <h2>
                             ${artistName} - ${trackName}
                         </h2>
-                        <p>Duration - ${trackDuration}</p>
+                        <p>Genre - ${genre}</p>
                     </div>
                 </div>
                 
-                <div class="play-pause-button-container js-play-pause-button-container" id="play-pause-button-container">
-                    <a><i class="fa-solid fa-play fa-3x js-play-pause-icon"></i></a>
+                <div class="play-pause-button-container js-play-pause-button-container" id="play-pause-button-container-${i}">
+                    <a><i class="fa-solid fa-play fa-3x js-play-pause-icon" id="play-pause-icon-${i}"></i></a>
                 </div>
             </div>
-        `; 
+        `;
 
         trackListHTML += html;
-        
     }
 
-    document.querySelector('.js-music-list')
-                .innerHTML = trackListHTML;
+    document.querySelector('.js-music-list').innerHTML = trackListHTML;
+
+    // toggle Play/Pause button 
+    document.querySelectorAll(".js-play-pause-icon").forEach((playPauseButton, index) => {
+        playPauseButton.addEventListener('click', () => {
+            console.log(`Click ${index}`);
+
+            if (playPauseButton.classList.contains("fa-pause")) {
+                playPauseButton.classList.remove("fa-pause");
+                playPauseButton.classList.add("fa-play");
+                pauseTrack();
+            } else {
+                pausePreviousSong();
+                playPauseButton.classList.remove("fa-play");
+                playPauseButton.classList.add("fa-pause");
+                activeButtonIndex = index;
+                playTrack(index);
+            }
+        });
+    });
 }
 
-//calling the render function
-renderTrackList();
-
-
-
-//toggle PlayPause button 
 let activeButtonIndex = null;
-document.querySelectorAll(".js-play-pause-icon").forEach((playPauseButton,index) => {
-    playPauseButton.addEventListener('click', () => {
-        console.log(`Click ${index}`);
-
-        if(index === activeButtonIndex) {
-            playPauseButton.classList.remove("fa-pause");
-            playPauseButton.classList.add("fa-play");
-            activeButtonIndex = null;
-        } else {
-            pausePreviousSong();
-            playPauseButton.classList.add("fa-pause");
-            playPauseButton.classList.remove("fa-play");
-            activeButtonIndex = index;
-        }
-
-
-    })
-});
 
 function pausePreviousSong() {
     if (activeButtonIndex !== null) {
-        const previousButton = document.querySelectorAll(".js-play-pause-icon")[activeButtonIndex];
+        const previousButton = document.querySelector(`#play-pause-icon-${activeButtonIndex}`);
         previousButton.classList.remove("fa-pause");
         previousButton.classList.add("fa-play");
-        console.log("this song is pause");
+        pauseTrack();
+        console.log("Previous song paused");
     }
 }
 
-//make a track to play and pause
-function playPauseTrack() {
-    
+function pauseTrack() {
+    // Logic to pause the currently playing track
 }
+
+function playTrack(index) {
+    // Logic to play the track at the specified index
+}
+
+// calling the render function
+renderTrackList();
